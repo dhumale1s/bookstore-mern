4c4960aa1cb28d70ca466c0169aa55ff1641d313
Ankit Dhumale <dhumale2ankit@gmail.com>
2024-08-03 23:22:26 +0530

    9th commit, frontend done but 2 pages remaining of contact us and About, we will do that later!

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,25 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Login from './Login'
+import { useForm } from "react-hook-form";
 
 function Signup() {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
         <>
             <div className='flex h-screen items-center justify-center'>
                 <div id="" className="w-[600px]">
                     <div className="modal-box">
-                        <form method="dialog">
+                        <form onSubmit={handleSubmit(onSubmit)} method="dialog">
                             {/* if there is a button in form, it will close the modal */}
                             <Link to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</Link>
-                        </form>
+                        
                         <h3 className="font-bold text-lg">SignUp</h3>
                         {/* Email */}
                         <div className='mt-4 space-y-2'>
                             <span>Name</span>
                             <br />
-                            <input type="email"
+                            <input type="name"
                                 placeholder='Enter your Full Name'
-                                className='w-80 px-3 py-1 border rounded-md outline-none' />
+                                className='w-80 px-3 py-1 border rounded-md outline-none' 
+                                {...register("name", { required: true })}
+                                />
+                                <br />
+                                {errors.name && <span className='text-sm text-red-500'>This field is required</span>}
                         </div>
                         {/* Email */}
                         <div className='mt-4 space-y-2'>
@@ -27,7 +36,11 @@ function Signup() {
                             <br />
                             <input type="email"
                                 placeholder='Enter your email'
-                                className='w-80 px-3 py-1 border rounded-md outline-none' />
+                                className='w-80 px-3 py-1 border rounded-md outline-none' 
+                                {...register("email", { required: true })}
+                                />
+                                <br />
+                                {errors.email && <span className='text-sm text-red-500'>This field is required</span>}
                         </div>
                         {/* password */}
                         <div className='mt-4 space-y-2'>
@@ -35,7 +48,11 @@ function Signup() {
                             <br />
                             <input type="text"
                                 placeholder='Enter your password'
-                                className='w-80 px-3 py-1 border rounded-md outline-none' />
+                                className='w-80 px-3 py-1 border rounded-md outline-none' 
+                                {...register("password", { required: true })}
+                                />
+                                <br />
+                                {errors.password && <span className='text-sm text-red-500'>This field is required</span>}
                         </div>
                         {/* Login Button */}
                         <div className='flex justify-around mt-4'>
@@ -48,6 +65,7 @@ function Signup() {
                             <Login/>
                             </p>
                         </div>
+                        </form>
                     </div>
                 </div>
             </div>
