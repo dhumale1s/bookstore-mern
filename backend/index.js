af1d284b46cfbd507f8eea7972ba68fe7d3073db
Ankit Dhumale <dhumale2ankit@gmail.com>
2024-08-04 22:19:17 +0530

    10th commit, backend done testing with postman, now time to start with axios in react

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+
+import bookRoute from "./route/book.route.js"
+
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const URI=process.env.MongDBURI;
+
+//connect to mongoDB
+try {
+    mongoose.connect(URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    console.log("Connected to mongoDB");
+} catch (error) {
+    console.log("Error: ",error)
+}
+
+//defining routes
+app.use("/book", bookRoute)
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+})
