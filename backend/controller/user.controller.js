f6f0a737c56299d575f5fc2b8f0944badc080337
Ankit Dhumale <dhumale2ankit@gmail.com>
2024-08-09 22:38:09 +0530

    13th commit, done with login backend part too, now frontend remaining

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -21,3 +21,23 @@ export const signup = async (req, res) => {
         res.status(500).json({message:"Internal server error"})
     }
 };
+
+export const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        const isMatch = await bcryptjs.compare(password, user.password)
+        if (!user || !isMatch) {
+            return res.status(400).json({message:"Ivalid username or password"});
+        }else{
+            return res.status(200).json({message:"Login successful", user:{
+                _id:user._id,
+                fullname:user.fullname,
+                email:user.email
+            }})
+        }
+    } catch (error) {
+        console.log("Error: " + error.message)
+        res.status(500).json({message:"Internal server error"})
+    }
+}
