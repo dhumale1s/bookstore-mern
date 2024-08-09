33c6b91e564e1f718b7f4bce23c835affd121903
Ankit Dhumale <dhumale2ankit@gmail.com>
2024-08-09 20:41:42 +0530

    11th commit, backend data is being accessed from mongodb successfully, now moving to login and signup authentication section.

--- frontend/src/components/Freebook.jsx
+++ frontend/src/components/Freebook.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-
 import list from '../../public/list.json';
 import Cards from './Cards';
+import axios from 'axios';
 
 function Freebook() {
-  const filterData = list.filter((data) => data.category === "Free");
-  console.log(filterData)
+  const[book,setBook]=useState([])
+    useEffect(()=>{
+        const getBook=async()=>{
+            try {
+                const res = await axios.get("http://localhost:4001/book");
+                console.log(res.data)
+                setBook(res.data.filter((data) => data.category === "Free"));
+            } catch (error) {
+                console.log(error)
+            }
+        };
+        getBook();
+    },[]);
+
+  //const filterData = list.filter((data) => data.category === "Free");
+  //console.log(filterData)
 
   var settings = {
     dots: true,
@@ -56,7 +69,7 @@ function Freebook() {
       
       <div>
         <Slider {...settings}>
-         {filterData.map((item)=>(
+         {book.map((item)=>(
           <Cards item={item} key={item.id}/>
          ))}
         </Slider>
